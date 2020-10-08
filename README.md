e18db0bafd904dc5e13f5e3c39065677a0300fdf
xrahoo <user@LAPTOP-HDSAJQQH.localdomain>
2020-10-09 01:51:25 +0300

    feature/markdown based parser, indendation normilization only

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # ctgov_parser
+
+flow
+1. iterate over every line
+2. first item - this is base indent, children of root
+3. save in stack last taken care of children's parent
+    a. if line has higher level, append as child of last item in stack
+    b. if line has lower level
+        - pop stack
+        - once hits a node with lower level then his, append as child
+    c. else (same level) put as brother (child of parent)
+
+no nodes - just render per case
+back indent - close the list and remember and indent index
+
+two steps
+1. indendenatation normalization
+2. tree building
+ mark down auto parsing
+
--- parser.go
+++ parser.go
@@ -3,6 +3,8 @@ package ctgov
 import (
 	"bufio"
 	"io"
+
+	"github.com/gomarkdown/markdown"
 )
 
 type Parser struct {
@@ -12,205 +14,47 @@ func NewParser() *Parser {
 	return &Parser{}
 }
 
-// parse ctgov raw textBlock into better looking html
-func (pa *Parser) Parse(r io.Reader) ([]byte, int) {
-
-	scanner := bufio.NewScanner(r)
-
-	var (
-		current *node
-		all     []node
-		stack   []node
-		idx     = 0
-	)
-
-	for scanner.Scan() {
-		line := scanner.Bytes()
-
-		n := parseLine(line)
-		n.htmlType = getOpenCloseTag(n.lineType)
-		n.idx = idx
-
-		idx++
-		switch n.lineType {
-		case emptyLine:
-
-			if current != nil {
-				if current.htmlType == p {
-					all = append(all, *current)
-					current = nil
-				} else if current.htmlType == co {
-					if len(stack) > 0 {
-						var temp = *current
-						for {
-							current, stack = &stack[len(stack)-1], stack[:len(stack)-1]
-
-							// can be just one comment
-							// replace it
-
-							lastChild := current.children[len(current.children)-1]
-							if len(lastChild.children) == 0 {
-								current.children[len(current.children)-1] = temp
-							} else {
-								grand := len(lastChild.children) - 1
-								current.children[len(current.children)-1].children[grand] = temp
-							}
-
-							//temp = *current
-							if len(stack) == 0 {
-
-								// done popping, here and before replace the current
-								break
-							}
-
-						}
-					}
-				}
-			}
-			break
-
-		case textLine, commentLine:
-
-			//if n.lineType == commentLine {
-			//	fmt.Println(idx, current)
-			//	fmt.Println(current.lineType, current.htmlType, len(current.contents), len(current.children))
-			//}
-
-			if current == nil {
-				current = &n
-			}
-
-			if current.htmlType == p {
-				current.contents = append(current.contents, n.content)
-			} else if current.htmlType == co {
-				current.contents = append(current.contents, n.content)
-			} else { // list
-
-				l := len(current.children) - 1
-				if n.textStart != current.textStart {
-
-					if n.textStart == current.textStart ||
-						(l != -1 && n.textStart == current.children[l].textStart) {
-
-						current.children[l].contents = append(current.children[l].contents, n.content)
-					} else if n.textStart < current.textStart ||
-						(l != -1 && n.textStart < current.children[l].textStart) { // back indent
-
-						if len(stack) > 0 {
-							var temp = *current
-							for {
-								current, stack = &stack[len(stack)-1], stack[:len(stack)-1]
-
-								// append the former-current to the current
-								current.children = append(current.children, temp)
-								temp = *current
-								if len(stack) == 0 {
-
-									// done popping, here and before replace the current
-									break
-								}
-
-							}
-						}
-
-						// before replace, add the current to the list..
-						all = append(all, *current)
-
-						pp := newParagraph(n)
-						current = &pp
-
-					} else {
-
-						//fmt.Println(len(stack), n.lineType)
-						if n.lineType == commentLine && len(stack) == 0 { // for comments and not parents,
-							pp := newComment(n)
-							current.children[l].children = append(current.children[l].children, pp)
-
-							stack = append(stack, *current)
-
-							current = &pp
-						} else { //
-							current.children[l].contents = append(current.children[l].contents, n.content)
-						}
-
-					}
-
-				} else {
-					current.children[l].contents = append(current.children[l].contents, n.content)
-				}
-			}
-
-			break
-
-		case dashLine, numberLine:
-
-			if current != nil {
-
-				if current.htmlType == p { // p after dash indicates replace
-
-					all = append(all, *current)
-
-					current = &n
-					current.contents = append(current.contents, n.content)
-
-				} else {
-
-					if n.textStart > current.textStart { // indent ul --> li - creating li
-						lli := newListItem(n)
-						n.children = append(n.children, lli)
-
-						stack = append(stack, *current)
-
-						current = &n
-
-					} else if n.textStart < current.textStart { // back indent
-						var temp = *current
-
-						// pop
-						for {
-							current, stack = &stack[len(stack)-1], stack[:len(stack)-1]
-
-							// append the former current to the current
-							current.children = append(current.children, temp)
-
-							if len(stack) == 0 {
-								break
-							}
-
-							temp = *current
-						}
-
-						lli := newListItem(n)
-
-						// append the new li
-						current.children = append(current.children, lli)
-
-					} else { // change type and adjust the content correctly
-
-						n.htmlType = li
-						n.contents = append(n.contents, n.content)
-						current.children = append(current.children, n)
-
-					}
-				}
-			} else { // no current, start one
-
-				if n.htmlType == ul || n.htmlType == ol {
-					lli := newListItem(n)
-					n.children = append(n.children, lli)
-					current = &n
-				}
-
-			}
-
+func calcIndentLevel(line []byte) int {
+	counter := 0
+	for i := 0; i < len(line); i++ {
+		if line[i] == ' ' {
+			counter = counter + 1
+		} else {
 			break
 		}
-
 	}
+	return counter
+}
 
-	if current != nil {
-		all = append(all, *current)
+func NormalizeIndent(r io.Reader) []byte {
+	scanner := bufio.NewScanner(r)
+	var (
+		baseIndent int
+		first      bool
+		retBuffer  []byte
+	)
+	first = true
+	for scanner.Scan() {
+		line := scanner.Bytes()
+		if first && len(line) > 0 {
+			baseIndent = calcIndentLevel(line)
+			first = false
+		} else if first {
+			continue
+		}
+		if len(line) > 0 {
+			line = line[baseIndent:]
+			retBuffer = append(retBuffer, string('\n')...)
+			retBuffer = append([]byte(retBuffer), []byte(line)...)
+		} else {
+			continue
+		}
 	}
+	return retBuffer
+}
 
-	return generateHtml(all), idx
+// parse ctgov raw textBlock into better looking html
+func (pa *Parser) Parse(r io.Reader) []byte {
+	buffer := NormalizeIndent(r)
+	return []byte(markdown.ToHTML(buffer, nil, nil))
 }
