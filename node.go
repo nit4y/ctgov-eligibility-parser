dbd38df0e9c4dd517d91d77d66b6850e4ccdb30b
xrahoo <user@LAPTOP-HDSAJQQH.localdomain>
2020-10-15 16:04:06 +0300

    Clean code improvements

--- node.go
+++ node.go
@@ -24,14 +24,18 @@ func calcNodeProps(line []byte, lastNode *node) (lineType, int) {
 	if len(line) > 0 {
 		for i := 0; i < len(line); i++ {
 			c := line[i]
+
 			if c == ' ' || c == '\t' { // tabs may appear as indentation as well
 				continue
+
 			} else if c >= '1' && c <= '9' {
 				if retType == unkLine { // still dont know, check to see if its number line
 					for j := i; j < len(line); j++ {
 						var cc = line[j]
+
 						if cc >= '0' && cc <= '9' {
 							continue
+
 						} else if cc == '.' {
 							start = i
 							if lastNode.lineType == emptyLine || lastNode.lineType == unkLine { // a text line might start with a numbering
@@ -40,12 +44,17 @@ func calcNodeProps(line []byte, lastNode *node) (lineType, int) {
 								retType = textLine
 							}
 							i = j
+
 							break
+
 						} else {
 							i = j
+
 							break
+
 						}
 					}
+
 				} else if retType == dashLine || retType == commentLine { // already know, quit
 					textStart = i
 					break
@@ -61,6 +70,7 @@ func calcNodeProps(line []byte, lastNode *node) (lineType, int) {
 			} else if c == '*' {
 				start = i
 				retType = commentLine
+
 			} else { // found text somewhere down the road
 
 				if start == 0 { // first time
@@ -68,30 +78,41 @@ func calcNodeProps(line []byte, lastNode *node) (lineType, int) {
 				}
 
 				textStart = i
+
 				if start == textStart { // text == start -> this is a text line
 					retType = textLine
 				}
+
 				break
 			}
 		}
+
 	} else {
 		retType = emptyLine
 	}
+
 	return retType, textStart
 }
 
 // calcLevel determines line "level" which is the number of indentations before the actual text is.
 func calcLevel(line []byte) int {
+
 	counter := 0
+
 	for i := 0; i < len(line); i++ {
+
 		if line[i] == '\t' {
 			counter = counter + 4 // tabs are equal to 4 spaces in terms of indention.
+
 		} else if line[i] == ' ' {
 			counter = counter + 1
+
 		} else {
 			break
+
 		}
 	}
+
 	return counter
 }
 
